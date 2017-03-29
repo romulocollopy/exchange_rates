3246d9a1a3bbc244f7a49b292c93772885f2114f
Rômulo Collopy <romulocollopy@gmail.com>
2017-03-29 18:06:34 -0300

    split js vendor code

--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,22 @@
 var path = require('path');
+var webpack = require("webpack");
 const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = function(env) {
     return {
         entry: {
             main: path.resolve(__dirname, 'static_src/js/app.js'),
-            vendor: 'highcharts'
+            vendor: ['highcharts', 'jquery', 'moment', 'lodash']
         },
         output: {
             filename: '[name].js',
             path: path.resolve(__dirname, 'project/static/js')
         },
         plugins: [
-            new UglifyJSPlugin()
+            new UglifyJSPlugin(),
+            new webpack.optimize.CommonsChunkPlugin(
+                {name: "vendor", filename:"vendor.bundle.js",
+                    minChunk: "infinity"})
           ]
     }
 }
